4fe24497f7052240055cf0a7f9f15ab3dad75f23
Larry Finger <Larry.Finger@lwfinger.net>
2014-02-05 13:50:49 -0600

    rtl8723be: Modify for API changes in kernel 3.14
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- ps.c
+++ ps.c
@@ -525,7 +525,7 @@ void rtl_swlps_beacon(struct ieee80211_hw *hw, void *data, unsigned int len)
 		return;
 
 	/* and only beacons from the associated BSSID, please */
-	if (compare_ether_addr(hdr->addr3, rtlpriv->mac80211.bssid))
+	if (ether_addr_equal(hdr->addr3, rtlpriv->mac80211.bssid))
 		return;
 
 	rtlpriv->psc.last_beacon = jiffies;
@@ -1005,7 +1005,7 @@ void rtl_p2p_info(struct ieee80211_hw *hw, void *data, unsigned int len)
 		return;
 
 	/* and only beacons from the associated BSSID, please */
-	if (compare_ether_addr(hdr->addr3, rtlpriv->mac80211.bssid))
+	if (ether_addr_equal(hdr->addr3, rtlpriv->mac80211.bssid))
 		return;
 	
 	/* check if this really is a beacon */
